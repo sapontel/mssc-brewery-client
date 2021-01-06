f9c3ae851a1bac7182dee1115d3df3de2ee06684
Saul Osbaldo Aponte Lopez <ingsoal@gmail.com>
2021-01-06 12:26:48 -0500

    Start project

---
+++ src/main/java/sos/com/msscbreweryclient/web/client/package-info.java
@@ -0,0 +1,1 @@
+package sos.com.msscbreweryclient.web.client;
---
+++ src/test/java/sos/com/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package sos.com.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
